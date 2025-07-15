f19ca8ce4587c9ce35dd9e2ebe3c39dcfed4928d
Zayn Mady <zeinmady2848@gmail.com>
2025-07-15 17:27:25 +0300

    added a navbar and implementation for main app UI. including a base for a homepage and a base for a project base

---
+++ frontend/src/pages/projects.jsx
@@ -0,0 +1,16 @@
+// src/pages/Projects.jsx
+import React from 'react';
+
+function Projects() {
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center p-8"
+      style={{ backgroundImage: "url('/whiteboard.jpg')" }}
+    >
+      <h1 className="text-3xl font-bold text-white mb-6">Your Projects</h1>
+    
+    </div>
+  );
+}
+
+export default Projects;
