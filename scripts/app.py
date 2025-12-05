b95bda3ed3ea7dfa761891e36209fdaa4a2e7e47
ZaynMady <zeinmady2848@gmail.com>
2025-12-05 22:39:24 +0200

    add Sentry SDK initialization to app setup

--- scripts/app.py
+++ scripts/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+import sentry_sdk
 
 
 def create_app():
 
     app = Flask(__name__)
+
+
+    sentry_sdk.init(
+        dsn="https://fa944fec2f507e6ea4b2c61dbe9c900c@o4510484098580480.ingest.de.sentry.io/4510484186595408",
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+    )
    
     CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
